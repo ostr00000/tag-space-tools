5820dcaa30817cb4134460570329391c1b7c57e2
ostr00000 <ostr00000@gmail.com>
2022-11-01 03:17:57 +0100

    use pyproject.toml configuration instead of setup.py,
    change `lib` to `src`,
    add gitignore,

---
+++ src/manager/actions/tag_space_fix.py
@@ -0,0 +1,14 @@
+from PyQt5.QtGui import QIcon
+from PyQt5.QtWidgets import QWidget
+
+from pyqt_utils.qobjects.display_widget_action import DisplayWidgetAction
+from tag_space_tools.gui.tagspace_tab_widget import TagSpaceTabWidget
+
+
+class PluginTagSpaceTools(DisplayWidgetAction):
+    def __init__(self, parent):
+        icon = QIcon.fromTheme('tagspaces')
+        super().__init__(icon=icon, text='Tag Space Tools', parent=parent)
+
+    def createWidget(self) -> QWidget:
+        return TagSpaceTabWidget()
